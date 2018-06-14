50f42d7e2c293db600ae0d8e19af11e7d91aefe1
Ran <ranraphaelnir@gmail.com>
2018-06-14 13:41:05 +0300

    remove iis stuff

--- client/run for iis sites.bat
+++
@@ -1,3 +0,0 @@
-C:\windows\system32\inetsrv\appcmd.exe add apppool /name:ikka /PipelineMode:intergrated /ManagedRuntimeVersion:v4.0 /enable32BitAppOnWin64:true
-
-C:\windows\system32\inetsrv\appcmd.exe add app /site.name:"default web site" /app.name:ikka /applicationpool:ikka /physicalpath:"%~dp0builds\development" /path:"/ikka" /enabledprotocols:http,https
