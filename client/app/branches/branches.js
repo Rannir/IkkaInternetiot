6bd03ce2305a6efa1336c2e959ba805f8a207d07
eden <sarisrael@gmail.com>
2018-06-20 01:43:46 +0300

    draw markers

--- client/app/branches/branches.js
+++ client/app/branches/branches.js
@@ -3,10 +3,11 @@
 angular.module('ikka').controller('branchesController', function($scope, $location, branchesService, consts) {
   const ctrl = this;
 
-  var myLatlng = new google.maps.LatLng((lat: 32.07), (lng: 34.79));
+  // Tel aviv latlng
+  var FocusLatLng = new google.maps.LatLng({lat: 32.07, lng: 34.79});
 
   $scope.map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatlng,
+    center: FocusLatLng,
     zoom: 8,
   });
 
@@ -15,12 +16,18 @@ angular.module('ikka').controller('branchesController', function($scope, $locati
     {id: 1, name: 'Baree', address: 'Azrieli', city: 'tel aviv', lat: 31.97, lng: 34.78},
   ];
 
-  var marker = new google.maps.Marker(myLatlng, (map: map));
-  marker.setMap(map);
-
   $scope.createMarkers = () => {
     for (var branch of $scope.branches) {
-      var marker = new google.maps.Marker({position: branch, map: map});
+      var marker = new google.maps.Marker({position: branch, map: $scope.map});
+      //marker.setMap($scope.map);
+      $scope.markers.add(marker);
+      alert('g');
+    }
+  };
+
+  $scope.clearMarkers = () => {
+    for (var marker of $scope.markers) {
+      marker.setMap(null);
     }
   };
 
