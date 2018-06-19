efe8ef29388415ceabb0bdbcff3fd35fe1a7e596
eden <sarisrael@gmail.com>
2018-06-20 02:27:03 +0300

    markers are created and filtered

--- client/app/branches/branches.js
+++ client/app/branches/branches.js
@@ -6,22 +6,28 @@ angular.module('ikka').controller('branchesController', function($scope, $locati
   // Tel aviv latlng
   var FocusLatLng = new google.maps.LatLng({lat: 32.07, lng: 34.79});
 
+  $scope.markers = [];
+
   $scope.map = new google.maps.Map(document.getElementById('map'), {
     center: FocusLatLng,
     zoom: 8,
   });
 
   $scope.branches = [
-    {id: 1, name: 'Eden', address: 'Azrieli', city: 'tel aviv', lat: 32.17, lng: 34.79},
-    {id: 1, name: 'Baree', address: 'Azrieli', city: 'tel aviv', lat: 31.97, lng: 34.78},
+    {id: 1, name: 'FashionMall', address: 'Rothshild 23', city: 'Tel aviv', lat: 32.17, lng: 34.79},
+    {id: 2, name: 'Baree', address: 'Azrieli', city: 'Tel aviv', lat: 31.97, lng: 34.78},
+    {id: 3, name: 'Igor', address: 'sderot katzir 2', city: 'Tel aviv', lat: 31.97, lng: 34.79},
+    {id: 4, name: 'ILoveFlex', address: 'golda 7', city: 'Ashdod', lat: 31.8, lng: 34.6434},
+    {id: 5, name: 'Angularia', address: 'sderot katzir 2', city: 'Raanana', lat: 32.1836, lng: 34.87386},
   ];
 
-  $scope.createMarkers = () => {
-    for (var branch of $scope.branches) {
+  $scope.createMarkers = branches => {
+    $scope.clearMarkers();
+
+    for (var branch of branches) {
       var marker = new google.maps.Marker({position: branch, map: $scope.map});
-      //marker.setMap($scope.map);
-      $scope.markers.add(marker);
-      alert('g');
+
+      $scope.markers.push(marker);
     }
   };
 
@@ -29,9 +35,12 @@ angular.module('ikka').controller('branchesController', function($scope, $locati
     for (var marker of $scope.markers) {
       marker.setMap(null);
     }
+
+    $scope.markers = [];
   };
 
-  //createMarkers(branches);
+  // Draw the hardcoded markers
+  $scope.createMarkers($scope.branches);
 
   return ctrl;
 
